6390505beecadce7d7af837de2a2503220df3b71
lucas emanoel <lucws2002@gmail.com>
2022-12-08 19:26:59 -0400

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,64 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+    private static Connection connection;
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")){
+            Properties props =  new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        if(connection == null) {
+            try {
+                Properties props = loadProperties();
+                String URL = props.getProperty("dburl");
+                connection = DriverManager.getConnection(URL, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
